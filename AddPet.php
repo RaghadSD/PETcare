36e521e31d300e3c57861bb5c9a7814fa21da857
Dalalghesh <Dalal.ghesh@gmail.com>
2022-05-11 02:27:56 +0300

    Update AddPet.php

--- AddPet.php
+++ AddPet.php
@@ -1,11 +1,9 @@
 <?php
-            if (!($database = mysqli_connect("localhost", "root", "")))
-                die("<p>Could not connect to database</p>");
+if (!($database = mysqli_connect("localhost", "root", "")))
+    die("<p>Could not connect to database</p>");
 
-            if (!mysqli_select_db($database, "petcare1"))
-                die("<p>Could not open URL database</p>");
-
-
+if (!mysqli_select_db($database, "petcare1"))
+    die("<p>Could not open URL database</p>");
 
             if (isset($_POST['Add'])) {
 
