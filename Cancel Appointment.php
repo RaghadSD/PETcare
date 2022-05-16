f0a2ef2d6284d78fce670f4c8448e5abae301318
Dalalghesh <Dalal.ghesh@gmail.com>
2022-05-16 09:30:16 +0300

    Cancel appointment

---
+++ Cancel Appointment.php
@@ -0,0 +1,34 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION['email']) ) { 
+    header("location: login-managers.php");
+    exit();
+}
+
+if (!($database = mysqli_connect("localhost", "root", "")))
+                die("<p>Could not connect to database</p>");
+
+            if (!mysqli_select_db($database, "petcare1"))
+                die("<p>Could not open URL database</p>");
+
+
+                if(isset($_GET['deleteid'])){
+                    $id=$_GET['deleteid'];
+
+                  $query="  DELETE FROM `appointment` WHERE id='$id'";
+                
+                  $r_update = mysqli_query($database, $query);
+                  if ($r_update) {                   
+                      echo "<script>alert('Appointment has been canceled successfully')</script>";
+                      header("location: Owner homepage.php");
+                  } else {
+                      echo "<script>alert('Error: Cannot cancel appointment!')</script>";
+                      echo  $database->error;
+                      exit();
+                  }
+
+                }
+                
+            
+?> 
