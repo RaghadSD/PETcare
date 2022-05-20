22e57590d334a9706ca339a743d97d88d1c94783
RaghadSD <86389094+RaghadSD@users.noreply.github.com>
2022-05-20 20:45:31 +0300

    login

--- Home.php
+++ Home.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'header.php'; 
         
     require "mail.php";
 
@@ -29,35 +30,6 @@ $result3 = mysqli_query($database,$query3);
 $rows2 = mysqli_fetch_array($result3);
 
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title> Pet care</title>
-    <meta charset = "utf-8">
-    <meta neme= "viewport" contenet="with=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href = "style.css"> 
-    
-</head>
-
-<body> 
-    
-<section class="header">
-<nav> 
-  <a href="Home.php"> <img id=logo src="Image (2).jpeg"></a>
-
-<div> 
-    <div class="header-links">
-
-        <ul>
-            <li> <a href="#ABOUTUS"> About </a> </li>
-           <!-- <li> <a href="#LoginOP"> Login </a> </li> --> 
-            <li> <a href="#services"> Services </a> </li>
-            <li> <a href="#contact"> Contact </a> </li>
-        </ul>
-
-    </div>
-    
-</div>
 
 <div class = "description"> 
 <h1>
