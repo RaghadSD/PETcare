84df8277104430ca2211ff6cb1059ade99341dac
Dalalghesh <Dalal.ghesh@gmail.com>
2022-05-21 07:33:31 +0300

    Update Cancel.php

--- Cancel.php
+++ Cancel.php
@@ -18,7 +18,7 @@ if (!($database = mysqli_connect("localhost", "root", "")))
                       echo  $database->error;
                       exit();
                   }
-                  header("location: Upcoming appointments.php");
+                  header("location: appointment requests .php");
 
 
                 
