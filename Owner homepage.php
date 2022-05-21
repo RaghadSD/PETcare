5cfee9ec32743616dcd99d48e8a33096bf8a9c10
aljoharash <88160746+aljoharash@users.noreply.github.com>
2022-05-21 03:20:13 +0300

    Update Owner homepage.php

--- Owner homepage.php
+++ Owner homepage.php
@@ -150,10 +150,13 @@ $rows2 = mysqli_fetch_array($result3);
             
        </section>
 
-   <section class="about-section" id="ABOUTUS">
+<<section class="about-section" id="ABOUTUS">
         <div class="box">
+
+            <?php if(mysqli_num_rows($result3)>0){?>
             <h1><?php echo $rows2['title'];?> </h1>
             <p class="about-content">
+            <img  class="aboutimg" style="height: 230px; width: 230px;" alt="about us picture" src="data:image/jpeg;base64, <?php echo base64_encode($rows2['picture']) ;?>">
             <?php echo $rows2['description'];?> 
             </p>
             <div class="words">
@@ -162,9 +165,13 @@ $rows2 = mysqli_fetch_array($result3);
                 <span ><a  href="tel:<?php echo $rows2['phoneNumber']; ?>" style="text-decoration: none; color: #617470; background-color: transparent ;"> <?php echo $rows2['phoneNumber']; ?></a></span>
                 <span ><a id ="words" href="mailto:<?php echo $rows2['email'];?>" style="text-decoration: none; color: #617470; background-color: transparent ;"><?php echo $rows2['email'];?> </a></span>
             </div>
+            <?php }?>
             <br> <br>
+            <a href="editAboutUs.php" class="editAbout"> Edit About us </a>
+
         </div>
     </section>
+
     <section id="contact">
 
         <h1 class="contact-header">Contact us!</h1>
@@ -198,6 +205,8 @@ $rows2 = mysqli_fetch_array($result3);
             <div class="contact-information"> <BR><BR> <BR><BR><BR><BR>
                 <hr>
                 <ul class="contact-list">
+                    <?php if(mysqli_num_rows($result3)>0){
+                        ?>
                     <li class="list-item"><span class="contact-text place"> <?php echo $rows2['location'];?></span></li>
 
                     <li class="list-item"><span class="contact-text phone"><a href="tel:<?php echo $rows2['phoneNumber']; ?>"> <?php echo $rows2['phoneNumber']; ?>
@@ -205,6 +214,7 @@ $rows2 = mysqli_fetch_array($result3);
 
                     <li class="list-item"><span class="contact-text email"><a href="mailto:<?php echo $rows2['email'];?>">
                                 <?php echo $rows2['email'];?> </a></span></li>
+                                <?php } ?>
                 </ul>
 
                 <hr>
@@ -214,6 +224,7 @@ $rows2 = mysqli_fetch_array($result3);
         </div>
 
     </section>
+
 
 </body>
 
